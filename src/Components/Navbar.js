10f8ba1a1b9ea762157c87c9c0f46911f5ef0a8b
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-17 17:15:23 +0530

    nav bar coller changing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
+
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
