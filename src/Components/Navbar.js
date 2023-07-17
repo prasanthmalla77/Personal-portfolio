e508c9829f95e5d244f3d39ee9d270cfec011e0f
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-17 17:41:14 +0530

    added navbar styling, nav bar successfully completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,21 +3,20 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
-
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
-  const [color,setColor]=useState(false);
-  const changeColor=()=>{
-    if(window.scrollY>=100){
-        setColor(true);
-    }else{
-        setColor(false);
+  const [color, setColor] = useState(false);
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else {
+      setColor(false);
     }
-  }
-  window.addEventListener("scroll",changeColor);
+  };
+  window.addEventListener("scroll", changeColor);
   return (
-    <div className={color? "header header-bg":"header"}>
+    <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
