e0f7db5e86736564732c5365c3afe8f342782429
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-17 17:52:53 +0530

    footer started

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+import { FaHome } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="foooter-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{color:'white', marginRight:"2rem"}}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 //rafce for automatiic functional component
 import Navbar from "../Components/Navbar";
 import HeroImg from "../Components/HeroImg";
+import Footer from "../Components/Footer";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroImg />
+      <Footer/>
     </div>
   );
 };
