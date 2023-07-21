ce56c9535cd473967cae5485f82eff513de0d309
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-21 18:32:45 +0530

    footer section start

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,33 @@
 import React from "react";
 import "./Footer.css";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaPhone ,FaMailBulk} from "react-icons/fa";
 const Footer = () => {
+    
   return (
     <div className="footer">
       <div className="foooter-container">
         <div className="left">
           <div className="location">
             <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <div>
+                <p>1-147,Venkateswara colony,Tekkali</p>
+                <p>Srikakulam,Andhra Pradesh,India </p>
+                <p>532201</p>
+            </div>
+          </div>
+          <div className="phone">
+          <FaPhone size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <div>
+                <h4>+91 9491920031</h4>
+            </div>
+
+          </div>
+          <div className="email">
+          <FaMailBulk size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <div>
+                <h4>prasanthmalla77@gmail.com</h4>
+            </div>
+
           </div>
         </div>
       </div>
