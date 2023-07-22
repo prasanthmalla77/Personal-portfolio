64275002026950296ba160fbed129f974d74eb13
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-22 15:48:44 +0530

    contact page

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './About.css';
 import Background from '../Components/Background';
 import Navbar from '../Components/Navbar';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const About = () => {
   const [formData, setFormData] = useState({
@@ -16,14 +18,10 @@ const About = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted!'); // Add this line to check if handleSubmit is triggered
-    // Rest of the code...
-  
-  
-    // Replace 'YOUR_FORMSPREE_ENDPOINT' with the Formspree endpoint
+    console.log('Form submitted!');
+
     const formEndpoint = 'https://formspree.io/f/mnqkngnd';
 
-    // Make a POST request to Formspree to send the form data to your email
     fetch(formEndpoint, {
       method: 'POST',
       headers: {
@@ -32,56 +30,96 @@ const About = () => {
       body: JSON.stringify(formData),
     })
       .then((response) => {
-        console.log('Form data sent successfully:', response);
-        // You can show a success message or redirect the user to a thank you page here
+        if (response.ok) {
+          toast.success('Form submitted successfully!', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          setFormData({ name: '', email: '', message: '' });
+        } else {
+          toast.error('Error submitting form. Please try again.', {
+            position: 'top-right',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
       })
       .catch((error) => {
         console.error('Error sending form data:', error);
-        // Handle errors, show error message, etc.
+        toast.error('Error submitting form. Please try again.', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       });
   };
 
   return (
     <div>
-    <Navbar/>
-    <Background/>
-    <div className='container'>
-    <form onSubmit={handleSubmit}>
-      <div>
-      
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
+      <Navbar />
+      <Background />
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form></div></div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
   );
 };
 
