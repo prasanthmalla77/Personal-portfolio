d53d09699a4387c8d1119cab94734119cb09d0c2
prasanthmalla77 <prasanthmalla77@gmail.com>
2023-07-22 16:34:55 +0530

    contact page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -28,14 +28,15 @@ const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
           <Link to="/project">Projects</Link>
         </li>
         <li>
           <Link to="/certification"> Certifications</Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
+        
         <li>
           <Link to="/contact">Contact</Link>
         </li>
